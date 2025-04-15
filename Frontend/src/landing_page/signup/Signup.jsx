8c798d243fba63af2adc4df81334ea5e2e67b3de
Achyuth Reddy <123964417+Achyuth1818@users.noreply.github.com>
2025-04-16 00:26:38 +0530

    Update Signup.jsx

--- Frontend/src/landing_page/signup/Signup.jsx
+++ Frontend/src/landing_page/signup/Signup.jsx
@@ -6,7 +6,7 @@ const Signup = () => {
       <div>
         <div className="flex gap-2 my-4 pb-2  border-b-2 ">
           <img
-            src="./public/Zerodhalogo.jpeg"
+            src="/Zerodhalogo.jpeg"
             alt="/public/Zerodhalogo.jpeg"
             className="w-[25px] h-[25px]"
           />
@@ -14,7 +14,7 @@ const Signup = () => {
         </div>
 
         <div className="mt-4  my-[5%] justify-center items-center md:mx-[20%]">
-          <img src="./public/homeHero.png" alt="public/homeHero.png" />
+          <img src="/homeHero.png" alt="public/homeHero.png" />
         </div>
       </div>
     </>
