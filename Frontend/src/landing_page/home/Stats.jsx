79d9a90c44bdf0c922315e0fe1f360c8a8b195fb
Achyuth Reddy <123964417+Achyuth1818@users.noreply.github.com>
2025-04-16 00:23:19 +0530

    Update Stats.jsx

--- Frontend/src/landing_page/home/Stats.jsx
+++ Frontend/src/landing_page/home/Stats.jsx
@@ -4,7 +4,7 @@ const Stats = () => {
   return (
     <>
       <div className="m-20">
-        <img src="./public/ecosystem.png" alt="/public/ecosystem.png" />
+        <img src="/ecosystem.png" alt="/public/ecosystem.png" />
       </div>
     </>
   );
