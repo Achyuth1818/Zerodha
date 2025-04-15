880d02b6ff454142ab9be807ab57de79d127b61a
Achyuth Reddy <123964417+Achyuth1818@users.noreply.github.com>
2025-04-16 00:12:40 +0530

    Update ProductPage.jsx

--- Frontend/src/landing_page/products/ProductPage.jsx
+++ Frontend/src/landing_page/products/ProductPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 
-import LeftSection from "./leftSection";
+import LeftSection from "./LeftSection";
 import Hero from "./Hero";
 import RightSection from "./RightSection";
 
